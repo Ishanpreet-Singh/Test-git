88ac60798f0aa757c21a27c699e59f5125f2c9d6
Ishanpreet-Singh <sin14799@sheridancollege.ca>
2026-02-05 16:48:19 -0500

    first commit

---
+++ src/main/java/com/mycompany/tdddemo/Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tdddemo;
+
+/**
+ *
+ * @author ishan
+ */
+public class Calculator {
+
+    double divide(double a, double b) {
+       //return 0;
+       double result;
+       result = a/b;
+       return result;
+    }
+
+       
+}
